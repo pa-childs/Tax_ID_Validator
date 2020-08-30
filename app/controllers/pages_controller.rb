95d6f9ba8dc2d28e03aca6c95a1cffa41eca59dc
Paul Childs <pachilds@gmail.com>
2020-08-30 10:35:55 -0400

    Add route for Validation and comments to PagesController with test Tax IDs.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,19 @@ class PagesController < ApplicationController
 
   def home
 
-    @countries = Countries.all
+    countries = Countries.all
+    @country_list = countries.collect { |item|
+      [item[:name], item[:alias]]
+    }
 
   end
 
   def validate
+
+    # Australin ABN Number: 51 824 753 556
+    # EU VAT Number:        DE345789003
+    #                       IE6388047V
+    #                       LU21416127
 
   end
 
